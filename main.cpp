e197c1a08eb08dccdd6c7e2c610f17fe4c918d16
Mms92 <martin-manche@hotmail.fr>
2018-06-09 19:14:37 +0200

    SDL Limit
    limite la fréquence d'image a 60 fps

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <sys/stat.h>
 #include <SDL2/SDL.h>
 #include <cstdio>
+#include <time.h>
 
 using namespace std;
 
@@ -42,21 +43,30 @@ int main(int argc, char *argv[]) {
 	//_rendererManager.UnloadChunk(c);
 
     bool running = true;
-
+    unsigned int pass = 0;
     SDL_Event event;
-
     while (running) {
-
+        pass++;
+        cout << pass << endl;
+        unsigned int time = SDL_GetTicks();
         while (SDL_PollEvent(&event)) {
 
 			switch (event.type) {
-
-			case SDL_QUIT:
-				running = false;
-				break;
+                case SDL_QUIT:
+                    running = false;
+                    break;
 			}
 		}
-        _rendererManager.Update();
+
+
+
+		_rendererManager.Update();
+
+        time = SDL_GetTicks() - time;
+
+
+        SDL_Delay( 1 / 60 * 1000 - time );
+
     }
 
 	return EXIT_SUCCESS;
